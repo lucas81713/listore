2e33f0870441c258ce9f48f32ae94b32c0d6a6f1
lucas81713 <lucasjiayi@gmail.com>
2026-01-30 17:41:48 -0500

    Fix delete button symbol in addItem function

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function addItem(text) {
   span.textContent = text;
 
   const del = document.createElement("button");
-  del.textContent = "✖";
+  del.textContent = "✕";
   del.className = "delete-btn";
 
   span.addEventListener("click", () => {
